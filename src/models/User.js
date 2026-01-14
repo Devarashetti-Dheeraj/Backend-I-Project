38bd7196036c84d553f38df9630286de754efeed
Devarashetti-Dheeraj <dheeraj2032006@gmail.com>
2026-01-15 01:26:34 +0530

    second commit

--- src/models/User.js
+++ src/models/User.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema(
     password: { type: String },
     role: { type: String, enum: ["Admin", "User"], default: "User" },
     authProvider: { type: String, enum: ["Local", "Google"], default: "Local" },
-    status: { type: String, enum: ["Active", "Inactive"], default: "Active" }
+    status: { type: String, enum: ["Active", "Inactive"], default: "Active" },
   },
   { timestamps: true }
 );
